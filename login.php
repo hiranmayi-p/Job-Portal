659bd83f61aaa802057d53e1229b50ae934bb145
hiranmayi-p <67116627+hiranmayi-p@users.noreply.github.com>
2020-10-19 21:43:06 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,20 @@
+<?ph<?php
+ $username = $_POST['username'];
+ $email = $_POST['email'];
+ $password = $_POST['password'];
+
+ // Database connection
+ $conn = new mysqli('localhost','root','','test');
+ if($conn->connect_error){
+  echo "$conn->connect_error";
+  die("Connection Failed : ". $conn->connect_error);
+ } else {
+  $stmt = $conn->prepare("insert into registration(username,email,password) values(?, ?, ?, ?, ?, ?)");
+  $stmt->bind_param("sssssi", $username, $email, $password);
+  $execval = $stmt->execute();
+  echo $execval;
+  echo "Registration successfully...";
+  $stmt->close();
+  $conn->close();
+ }
+?>p
